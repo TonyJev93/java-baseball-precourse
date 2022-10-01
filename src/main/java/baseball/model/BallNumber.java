51c8ce7327d58a3c4a012ddd2d9388be460e70be
JUN GYEWON <tonyjev93@gmail.com>
2022-10-02 08:44:59 +0900

    feat: 래퍼 클래스(Ball, BallNumber, BallPosition) 생성 및 기능 구현

---
+++ src/main/java/baseball/model/BallNumber.java
@@ -0,0 +1,28 @@
+package baseball.model;
+
+public class BallNumber {
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 9;
+
+    private int value;
+
+    private BallNumber(int value) {
+        this.value = value;
+    }
+
+    public static BallNumber from(int value) {
+        validate(value);
+
+        return new BallNumber(value);
+    }
+
+    private static void validate(int value) {
+        if (value < MIN_NUMBER || value > MAX_NUMBER) {
+            throw new IllegalArgumentException("공 위치는 1~9 사이 값만 생성 가능합니다.");
+        }
+    }
+
+    public int value() {
+        return value;
+    }
+}
